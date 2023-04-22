e2088fe5f01015b99c665531c6e27c0007ae1432
Azizbek Akhmadov <81019633+Azizbek-Akhmadov@users.noreply.github.com>
2023-04-23 05:29:01 +0900

    Update generate_cached_setup.py

--- Mid-term Incheon city/build/catkin_generated/generate_cached_setup.py
+++ Mid-term Incheon city/build/catkin_generated/generate_cached_setup.py
@@ -12,7 +12,7 @@ try:
     from catkin.environment_cache import generate_environment_script
 except ImportError:
     # search for catkin package in all workspaces and prepend to path
-    for workspace in '/home/james77/Desktop/SMS-Lab/Mid_term-Project/smart_city/devel;/opt/ros/noetic'.split(';'):
+    for workspace in '/Azizbek'.split(';'):
         python_path = os.path.join(workspace, 'lib/python3/dist-packages')
         if os.path.isdir(os.path.join(python_path, 'catkin')):
             sys.path.insert(0, python_path)
@@ -21,7 +21,7 @@ except ImportError:
 
 code = generate_environment_script('/home/james77/Desktop/SMS-Lab/Mid_term-Project/smart_city/devel/env.sh')
 
-output_filename = '/home/james77/Desktop/SMS-Lab/Mid_term-Project/smart_city/build/catkin_generated/setup_cached.sh'
+output_filename = 'Azizbek
 with open(output_filename, 'w') as f:
     # print('Generate script for cached setup "%s"' % output_filename)
     f.write('\n'.join(code))
